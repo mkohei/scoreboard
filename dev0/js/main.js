b1cdfcc81c5ea71a96f88d5211b447c970e82bef
mkohei <baberugaberiimeronnv5@gmail.com>
2018-08-31 15:15:55 +0900

    個人的に完成
    あとはchobi修正

--- dev0/js/main.js
+++ dev0/js/main.js
@@ -1,5 +1,7 @@
 var labs = { shimizu: "清水", shinomori: "篠森", kurihara: "栗原", nakahara: "中原", hamamura: "濱村", fukumoto: "福本", yokoyama: "横山" };
-var games = { mk: "マリオカート8", sb: "大乱闘スマッシュブラザーズ", gb: "Gang Beasts" }
+var games = { mk: "マリオカート8", sb: "大乱闘スマッシュブラザーズ", gb: "Gang Beasts" };
+var ordercolor = ['#FFD70080', '#C0C0C080', '#C4722280'];
+var orderimg = ['src/1-3.png', 'src/2.png', 'src/3.png'];
 
 var scorelist = {};
 
@@ -16,8 +18,38 @@ function deco_order() {
     for (key in scorelist) {
         scorevals.push(scorelist[key]);
     }
-    scorevals.sort();
+    var scorevals = Array.from(new Set(scorevals));
+    console.log(scorevals);
+    scorevals.sort(function(a, b) {
+        if (a < b) return 1;
+        if (a > b) return -1;
+        return 0;
+    });
+    console.log(scorevals);
+    cnt = 0;
+    for (i in scorevals) {
+        for (key in scorelist) {
+            if (scorelist[key] == scorevals[i]) {
+                var ele = document.getElementById(key);
+                ele.style.backgroundColor = ordercolor[i];
+                var ele = document.getElementById(key + "-order-deco");
+                ele.src = orderimg[i];
+                cnt++;
+            }
+        }
+        console.log(i, cnt);
+        if (cnt > 2) break;
+    }
+}
 
+function deco_null() {
+    for (lab in labs) {
+        var ele = document.getElementById(lab);
+        ele.style.backgroundColor = '#FFFFFFFF';
+        console.log(lab + "-order-deco");
+        var ele = document.getElementById(lab + "-order-deco");
+        ele.src = 'src/white.png';
+    }
 }
 
 
@@ -32,12 +64,14 @@ function update_score() {
                 sum += scores[game];
             }
             scorelist[lab] = sum;
+            console.log(lab, sum);
             document.getElementById(lab + "-sumscore").innerText = sum;
             document.getElementById(lab).setAttribute("data-myorder", sum);
         }
-        deco_order();
     });
 }
 
 
 document.getElementById("update-button").onclick = update_score;
+document.getElementById("default-order").onclick = deco_null;
+document.getElementById("ranking-order").onclick = deco_order;
